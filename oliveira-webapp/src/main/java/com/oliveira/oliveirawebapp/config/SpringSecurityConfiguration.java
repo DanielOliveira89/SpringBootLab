bba8d3336eaddccfaaee5b50f1146435c2a1d1fa
Oliveira <daniel-martins.oliveira@capgemini.com>
2023-07-12 09:04:22 +0100

    Setting up multiple users

--- oliveira-webapp/src/main/java/com/oliveira/oliveirawebapp/config/SpringSecurityConfiguration.java
+++ oliveira-webapp/src/main/java/com/oliveira/oliveirawebapp/config/SpringSecurityConfiguration.java
@@ -16,18 +16,26 @@ public class SpringSecurityConfiguration {
 	
 	@Bean
 	public InMemoryUserDetailsManager createUserManager() {
+		
+		UserDetails userDetails1 = createUser("daniel", "daniel");
+		UserDetails userDetails2 = createUser("maria", "maria");
+		
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+		
+	}
+
+
+	private UserDetails createUser(String password, String username) {
 		Function<String, String> passwordEncoder
-			= input -> passwordEncoder().encode(input);
+		= input -> passwordEncoder().encode(input);
 		
 		UserDetails userDetails = User.builder()
 										.passwordEncoder(passwordEncoder)
-										.username("daniel")
-										.password("daniel")
+										.username(username)
+										.password(password)
 										.roles("USER", "ADMIN")
 										.build();
-		
-		return new InMemoryUserDetailsManager(userDetails);
-		
+		return userDetails;
 	}
 	
 	
