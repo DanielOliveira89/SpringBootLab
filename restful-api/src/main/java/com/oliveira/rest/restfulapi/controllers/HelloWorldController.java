e3ba6af1adeb92d92f072b92a56192f916e44f29
DanielOliveira89 <dmartins.pessoal@gmail.com>
2023-07-13 23:05:04 +0100

    #2 - Hello world with JSON

--- restful-api/src/main/java/com/oliveira/rest/restfulapi/controllers/HelloWorldController.java
+++ restful-api/src/main/java/com/oliveira/rest/restfulapi/controllers/HelloWorldController.java
@@ -3,12 +3,19 @@ package com.oliveira.rest.restfulapi.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oliveira.rest.restfulapi.beans.HelloWorldBean;
+
 @RestController
 public class HelloWorldController {
 	
 	@GetMapping(path="/hello")
 	public String helloWorld() {
 		return "Hello, Sir!";
+	}
+	
+	@GetMapping(path="/hello-json")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean ("Hello, Sir!");
 	}
 
 }
