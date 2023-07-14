187440748aebfa4612bcbce8ba490713d6190dfe
DanielOliveira89 <dmartins.pessoal@gmail.com>
2023-07-14 19:06:47 +0100

    #4 - Adding ResponseEntity and location http header

--- restful-api/src/main/java/com/oliveira/rest/restfulapi/resources/UserResource.java
+++ restful-api/src/main/java/com/oliveira/rest/restfulapi/resources/UserResource.java
@@ -1,12 +1,15 @@
 package com.oliveira.rest.restfulapi.resources;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.oliveira.rest.restfulapi.beans.User;
 import com.oliveira.rest.restfulapi.dao.UserDaoService;
@@ -33,9 +36,14 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user){
+	public ResponseEntity<User> createUser(@RequestBody User user){
+		User createdUser = userService.save(user);
 		
-		return userService.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+						.path("/{id}")
+						.buildAndExpand(createdUser.getId())
+						.toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
