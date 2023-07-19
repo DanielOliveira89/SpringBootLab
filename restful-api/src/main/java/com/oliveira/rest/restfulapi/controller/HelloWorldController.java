203e04ea306f4f51cd11a59e79584b649faae2c6
DanielOliveira89 <dmartins.pessoal@gmail.com>
2023-07-19 09:43:28 +0100

    #11 - Internationalization with lang header

--- restful-api/src/main/java/com/oliveira/rest/restfulapi/controller/HelloWorldController.java
+++ restful-api/src/main/java/com/oliveira/rest/restfulapi/controller/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.oliveira.rest.restfulapi.controller;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +13,22 @@ import com.oliveira.rest.restfulapi.beans.HelloWorldBean;
 @RestController
 public class HelloWorldController {
 	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
 	@GetMapping(path="/hello")
 	public String helloWorld() {
 		return "Hello, Sir!";
+	}
+	
+	@GetMapping(path="/hello-lang")
+	public String helloWorldLang() {
+		Locale locale = LocaleContextHolder.getLocale();
+		
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 	
 	@GetMapping(path="/hello-json")
