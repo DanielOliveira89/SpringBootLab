0597bcac7ca2415b903391a1a62f9a509432a95e
DanielOliveira89 <dmartins.pessoal@gmail.com>
2023-07-19 11:08:42 +0100

    #12 - All kinds of versioning

---
+++ restful-api/src/main/java/com/oliveira/rest/restfulapi/beans/v2/PersonV2.java
@@ -0,0 +1,26 @@
+package com.oliveira.rest.restfulapi.beans.v2;
+
+public class PersonV2 {
+	private Name name;
+
+	public PersonV2(Name name) {
+		super();
+		this.name = name;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonV2 [name=" + name + "]";
+	}
+	
+	
+
+}
